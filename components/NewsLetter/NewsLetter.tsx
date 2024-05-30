94290224d889c31b39471a8a36e564d4d30b07ce
chell0 <machelgabriel@gmail.com>
2024-05-31 02:48:31 +0300

    Update the Newsletter section

--- components/NewsLetter/NewsLetter.tsx
+++ components/NewsLetter/NewsLetter.tsx
@@ -5,11 +5,11 @@ export default function NewsLetter() {
         <>
             <div className={`p-6 grid grid-rows-2 items-center justify-center max-w-lg bg-white shadow-xl rounded-2xl sm:px-4 mt-20`}>
                 <div className={`p-2 mt-2`}>
-                    <p className={`text-gray-600 text-3xl md:text-2xl sm:text-sm font-bold`}>Subscribe to join our Newsletter</p>
+                    <p className={`text-gray-600 text-3xl md:text-2xl sm:text-sm font-bold`}>Subscribe to Our Newsletter</p>
                 </div>
                 <div className={`flex flex-row justify-center items-center`}>
                     <Link href={`/`} className={`bg-orange-500 hover:bg-orange-200 hover:text-orange-400 font-semibold text-white text-lg no-underline py-2 px-6 rounded-xl`}>
-                        Join Now
+                        Sign Up
                     </Link>
                 </div>
             </div>
