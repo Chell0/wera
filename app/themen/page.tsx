ef85c017dccd7925bc3d197d69c153110a65ae8a
chell0 <machelgabriel@gmail.com>
2024-06-08 15:53:27 +0300

    - Remove the categories - Replay categories with tags - Use tags to filter blogs

--- app/themen/page.tsx
+++ app/themen/page.tsx
@@ -1,64 +1,65 @@
 "use client"
-import { useState } from 'react';
-import Category from "@/components/Category/Category";
+import { useState } from "react";
+import Tags from "@/components/Tags/Tags";
 import Image from "next/image";
 import Link from "next/link";
 import NavBar from "@/components/NavBar/NavBar";
 import Footer from "@/components/Footer/Footer";
-import Pagination from '@/components/Pagination/Pagination';
+import Pagination from "@/components/Pagination/Pagination";
 
 
 
 export default function BlogPage() {
-    const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+    const [selectedTag, setSelectedTag] = useState<string | null>(null);
     const [currentPage, setCurrentPage] = useState(1);
 
-    const handleCategorySelect = (category: string | null) => {
-        setSelectedCategory(category);
+    const handleTagSelect = (tag: string | null) => {
+        setSelectedTag(tag);
+        setCurrentPage(1);
     };
 
     const allBlogs = [
-        { id: 1, title: 'Blog Post 1', category: 'Category 1', image: '/banner.png', tags: ['tag1', 'tag2'] },
-        { id: 2, title: 'Blog Post 2', category: 'Category 2', image: '/banner.png', tags: ['tag2', 'tag3'] },
-        { id: 3, title: 'Blog Post 3', category: 'Category 3', image: '/banner.png', tags: ['tag1', 'tag3'] },
-        { id: 4, title: 'Blog Post 4', category: 'Category 4', image: '/banner.png', tags: ['tag1', 'tag4'] },
-        { id: 5, title: 'Blog Post 5', category: 'Category 1', image: '/banner.png', tags: ['tag1', 'tag2'] },
-        { id: 6, title: 'Blog Post 6', category: 'Category 2', image: '/banner.png', tags: ['tag2', 'tag3'] },
-        { id: 7, title: 'Blog Post 7', category: 'Category 3', image: '/banner.png', tags: ['tag1', 'tag3'] },
-        { id: 8, title: 'Blog Post 8', category: 'Category 4', image: '/banner.png', tags: ['tag1', 'tag4'] },
-        { id: 9, title: 'Blog Post 9', category: 'Category 1', image: '/banner.png', tags: ['tag1', 'tag2'] },
-        { id: 10, title: 'Blog Post 10', category: 'Category 2', image: '/banner.png', tags: ['tag2', 'tag3'] },
-        { id: 11, title: 'Blog Post 11', category: 'Category 3', image: '/banner.png', tags: ['tag1', 'tag3'] },
-        { id: 12, title: 'Blog Post 12', category: 'Category 4', image: '/banner.png', tags: ['tag1', 'tag4'] },
-        { id: 13, title: 'Blog Post 13', category: 'Category 1', image: '/banner.png', tags: ['tag1', 'tag2'] },
-        { id: 14, title: 'Blog Post 14', category: 'Category 2', image: '/banner.png', tags: ['tag2', 'tag3'] },
-        { id: 15, title: 'Blog Post 15', category: 'Category 3', image: '/banner.png', tags: ['tag1', 'tag3'] },
-        { id: 16, title: 'Blog Post 16', category: 'Category 4', image: '/banner.png', tags: ['tag1', 'tag4'] },
-        { id: 17, title: 'Blog Post 17', category: 'Category 1', image: '/banner.png', tags: ['tag1', 'tag2'] },
-        { id: 18, title: 'Blog Post 18', category: 'Category 2', image: '/banner.png', tags: ['tag2', 'tag3'] },
-        { id: 19, title: 'Blog Post 19', category: 'Category 3', image: '/banner.png', tags: ['tag1', 'tag3'] },
-        { id: 20, title: 'Blog Post 20', category: 'Category 4', image: '/banner.png', tags: ['tag1', 'tag4'] },
-        { id: 21, title: 'Blog Post 21', category: 'Category 1', image: '/banner.png', tags: ['tag1', 'tag2'] },
-        { id: 22, title: 'Blog Post 22', category: 'Category 2', image: '/banner.png', tags: ['tag2', 'tag3'] },
-        { id: 23, title: 'Blog Post 23', category: 'Category 3', image: '/banner.png', tags: ['tag1', 'tag3'] },
-        { id: 24, title: 'Blog Post 24', category: 'Category 4', image: '/banner.png', tags: ['tag1', 'tag4'] },
+        { id: 1, title: 'Blog Post 1', category: 'Category 1', image: '/banner.png', tags: ['stadtraum', 'verwaltung'] },
+        { id: 2, title: 'Blog Post 2', category: 'Category 2', image: '/banner.png', tags: ['verwaltung', 'bildung'] },
+        { id: 3, title: 'Blog Post 3', category: 'Category 3', image: '/banner.png', tags: ['stadtraum', 'bildung'] },
+        { id: 4, title: 'Blog Post 4', category: 'Category 4', image: '/banner.png', tags: ['stadtraum', 'veranstaltung'] },
+        { id: 5, title: 'Blog Post 5', category: 'Category 1', image: '/banner.png', tags: ['stadtraum', 'verwaltung'] },
+        { id: 6, title: 'Blog Post 6', category: 'Category 2', image: '/banner.png', tags: ['verwaltung', 'bildung'] },
+        { id: 7, title: 'Blog Post 7', category: 'Category 3', image: '/banner.png', tags: ['stadtraum', 'bildung'] },
+        { id: 8, title: 'Blog Post 8', category: 'Category 4', image: '/banner.png', tags: ['stadtraum', 'veranstaltung'] },
+        { id: 9, title: 'Blog Post 9', category: 'Category 1', image: '/banner.png', tags: ['stadtraum', 'verwaltung'] },
+        { id: 10, title: 'Blog Post 10', category: 'Category 2', image: '/banner.png', tags: ['verwaltung', 'bildung'] },
+        { id: 11, title: 'Blog Post 11', category: 'Category 3', image: '/banner.png', tags: ['stadtraum', 'bildung'] },
+        { id: 12, title: 'Blog Post 12', category: 'Category 4', image: '/banner.png', tags: ['stadtraum', 'veranstaltung'] },
+        { id: 13, title: 'Blog Post 13', category: 'Category 1', image: '/banner.png', tags: ['stadtraum', 'verwaltung'] },
+        { id: 14, title: 'Blog Post 14', category: 'Category 2', image: '/banner.png', tags: ['verwaltung', 'bildung'] },
+        { id: 15, title: 'Blog Post 15', category: 'Category 3', image: '/banner.png', tags: ['stadtraum', 'bildung'] },
+        { id: 16, title: 'Blog Post 16', category: 'Category 4', image: '/banner.png', tags: ['stadtraum', 'veranstaltung'] },
+        { id: 17, title: 'Blog Post 17', category: 'Category 1', image: '/banner.png', tags: ['stadtraum', 'verwaltung'] },
+        { id: 18, title: 'Blog Post 18', category: 'Category 2', image: '/banner.png', tags: ['verwaltung', 'bildung'] },
+        { id: 19, title: 'Blog Post 19', category: 'Category 3', image: '/banner.png', tags: ['stadtraum', 'bildung'] },
+        { id: 20, title: 'Blog Post 20', category: 'Category 4', image: '/banner.png', tags: ['stadtraum', 'veranstaltung'] },
+        { id: 21, title: 'Blog Post 21', category: 'Category 1', image: '/banner.png', tags: ['stadtraum', 'verwaltung'] },
+        { id: 22, title: 'Blog Post 22', category: 'Category 2', image: '/banner.png', tags: ['verwaltung', 'bildung'] },
+        { id: 23, title: 'Blog Post 23', category: 'Category 3', image: '/banner.png', tags: ['stadtraum', 'bildung'] },
+        { id: 24, title: 'Blog Post 24', category: 'Category 4', image: '/banner.png', tags: ['stadtraum', 'veranstaltung'] },
         // Add more blog posts here...
     ];
 
-    const filteredBlogs = selectedCategory
-        ? allBlogs.filter(blog => blog.category === selectedCategory)
+    const filteredBlogs = selectedTag
+        ? allBlogs.filter(blog => blog.tags.includes(selectedTag))
         : allBlogs;
 
-    const ITEMS_PER_PAGE = 9;
+    const ITEMS_PER_PAGE = 12;
 
-    const totalPages = Math.ceil(allBlogs.length / ITEMS_PER_PAGE);
+    const totalPages = Math.ceil(filteredBlogs.length / ITEMS_PER_PAGE);
 
     const handlePageChange = (page: number) => {
         setCurrentPage(page);
     };
 
     const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-    const currentBlogs = allBlogs.slice(startIndex, startIndex + ITEMS_PER_PAGE);
+    const currentBlogs = filteredBlogs.slice(startIndex, startIndex + ITEMS_PER_PAGE);
 
 
     return (
@@ -68,9 +69,9 @@ export default function BlogPage() {
                 <main className={`p-6`}>
                     <NavBar />
                     <div className="container mx-auto p-4">
-                        <Category onCategorySelect={handleCategorySelect} />
+                        <Tags onTagSelect={handleTagSelect} />
                         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
-                            {filteredBlogs.map(blog => (
+                            {currentBlogs.map(blog => (
                                 <div key={blog.id} className="bg-white rounded-lg shadow-md overflow-hidden">
                                     <Image src={blog.image} alt={blog.title} width={2000} height={870} className={`object-contain`} />
                                     <div className="p-3">
@@ -92,7 +93,13 @@ export default function BlogPage() {
                             ))}
                         </div>
                     </div>
-                    <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+                    {totalPages > 1 && (
+                        <Pagination
+                            currentPage={currentPage}
+                            totalPages={totalPages}
+                            onPageChange={handlePageChange}
+                        />
+                    )}
                     <Footer />
                 </main >
             </div >
---
+++ components/Tags/Tags.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+
+
+const tags = [
+    'stadtraum',
+    'verwaltung',
+    'bildung',
+    'veranstaltung',
+    'tag 5',
+    'tag 6',
+    'tag 7',
+    'tag 8'
+];
+
+interface TagsProps {
+    onTagSelect: (selectedTag: string | null) => void;
+}
+
+
+export default function Tags({ onTagSelect }: TagsProps) {
+    const [selectedTag, setSelectedTag] = useState<string | null>(null);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const handleTagClick = (tag: string | null) => {
+        setSelectedTag(tag);
+        onTagSelect(tag);
+        setIsDropdownOpen(false);
+    };
+
+    const handleDropdownToggle = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    return (
+        <>
+            <div className="hidden md:flex flex-wrap gap-4 mb-4 mt-20">
+                <button
+                    onClick={() => handleTagClick(null)}
+                    className={`
+                        ${selectedTag === null ? 'bg-orange-500 text-white' : 'bg-gray-200 text-gray-700'
+                        } rounded-full py-2 px-4`}
+                >
+                    View All
+                </button>
+                {tags.map((tag, index) => (
+                    <button
+                        key={index}
+                        onClick={() => handleTagClick(tag)}
+                        className={`${selectedTag === tag ? 'bg-orange-500 text-white' : 'bg-gray-200 text-gray-700'} rounded-full py-2 px-4`}
+                    >
+                        {tag}
+                    </button>
+                ))}
+            </div>
+            <div className="md:hidden mb-4">
+                <button
+                    onClick={handleDropdownToggle}
+                    className="bg-gray-200 text-gray-700 py-2 px-4 rounded-full w-full text-left">{selectedTag ? selectedTag : 'Select Tag'}
+                </button>
+                {isDropdownOpen && (
+                    <div className="bg-white border rounded-md mt-2 shadow-lg">
+                        <button
+                            onClick={() => handleTagClick(null)}
+                            className={`block w-full text-left px-4 py-2 ${selectedTag === null ? 'bg-orange-500 text-white' : 'bg-gray-200'}`}
+                        >
+                            View All
+                        </button>
+                        {tags.map((tag, index) => (
+                            <button
+                                key={index}
+                                onClick={() => handleTagClick(tag)}
+                                className={`block w-full text-left px-4 py-2 ${selectedTag === tag ? 'bg-orange-500 text-white' : 'text-gray-700'}`}>{tag}
+                            </button>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </>
+    );
+};
