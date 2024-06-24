a07edfae81ea96340a799d0c05b24d7dc6d513ef
chell0 <machelgabriel@gmail.com>
2024-06-25 01:48:04 +0300

    Update the Material Section

--- components/Material/MaterialCard.tsx
+++ components/Material/MaterialCard.tsx
@@ -1,27 +1,27 @@
-// import Image from "next/image";
-// import Link from "next/link";
+import Image from "next/image";
+import Link from "next/link";
 
-// type MaterialCardProps = {
-//     imageUrl: string,
-//     title: string,
-//     description: string,
-//     link: string,
-// }
+type MaterialCardProps = {
+    imageUrl: string,
+    title: string,
+    description: string,
+    link: string,
+}
 
-// export default function MaterialCard({ imageUrl, description, link }: MaterialCardProps) {
-//     return (
-//         <>
-//             <div className="max-w-screen-lg rounded-lg shadow-lg overflow-hidden">
-//                 <div className="relative w-full h-32">
-//                     <Image src={imageUrl} alt="Blog Image" layout="fill" objectFit="cover" />
-//                 </div>
-//                 <div className="bg-white p-5">
-//                     <p className="text-left text-sm mb-4 line-clamp-3">{description}</p>
-//                     <Link href="/" target="_blank" rel="noopener noreferrer" className={`bg-blue-500 hover:bg-orange-500 hover:text-white font-md text-white text-md no-underline py-2 px-4 rounded-xl`}>
-//                         {link}
-//                     </Link>
-//                 </div>
-//             </div>
-//         </>
-//     )
-// }
+export default function MaterialCard({ imageUrl, description, link }: MaterialCardProps) {
+    return (
+        <>
+            <div className="max-w-screen-lg rounded-lg shadow-lg overflow-hidden">
+                <div className="relative w-full h-32">
+                    <Image src={imageUrl} alt="Blog Image" layout="fill" objectFit="cover" />
+                </div>
+                <div className="bg-white p-5">
+                    <p className="text-left text-sm mb-4 line-clamp-3">{description}</p>
+                    <Link href="/" target="_blank" rel="noopener noreferrer" className={`bg-blue-500 hover:bg-orange-500 hover:text-white font-md text-white text-md no-underline py-2 px-4 rounded-xl`}>
+                        {link}
+                    </Link>
+                </div>
+            </div>
+        </>
+    )
+}
