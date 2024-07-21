7fd5df47cd23ab2118aac0165390fb970cd34f6b
chell0 <machelgabriel@gmail.com>
2024-07-22 01:42:57 +0300

    Update material page

--- components/BlogCard/BlogCard.tsx
+++ components/BlogCard/BlogCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+
 import { ThemenCard } from "@/app/lib/interface";
 import { urlFor } from "@/app/lib/sanityClient";
 
--- components/MaterialCards/MaterialCards.tsx
+++ components/MaterialCards/MaterialCards.tsx
@@ -4,13 +4,15 @@ import Link from "next/link";
 import { MaterialCard } from "@/app/lib/interface";
 import { urlFor } from "@/app/lib/sanityClient";
 
+
 export default function MaterialCards({ card }: { card: MaterialCard }) {
+
   const { title, titleImage, smallDescription, links } = card;
 
   return (
     <>
-      <div className="bg-amber-50 h-[380px] max-w-screen-lg rounded-lg shadow-md overflow-hidden">
-        <div className="bg-white flex justify-center items-center h-40">
+      <div className="bg-amber-50 h-[450px] max-w-screen-lg rounded-lg shadow-lg overflow-hidden">
+        <div className="bg-white flex justify-center items-center relative w-full h-56">
           <Image
             src={urlFor(titleImage).url()}
             alt={title}
@@ -18,21 +20,20 @@ export default function MaterialCards({ card }: { card: MaterialCard }) {
             width={150}
             style={{ maxHeight: `240px` }}
             quality={100}
-            className={`rounded-lg object-cover overflow-hidden`}
+            className={`rounded-t-lg object-cover overflow-hidden`}
           />
         </div>
-        <div className="bg-amber-50 flex flex-col justify-center items-center m-5">
-          <h3 className="text-orange-500 text-sm font-semibold mb-2">
+        <div className="bg-amber-50 p-5">
+          <h3 className="text-orange-500 text-lg font-semibold mb-3 cursor-pointer hover:underline line-clamp-4">
             <Link
               href={links}
               target="_blank"
               rel="noopener noreferrer"
-              className={`cursor-pointer no-underline hover:underline`}
             >
               {title}
             </Link>
           </h3>
-          <p className="text-gray-600 text-left text-xs mb-3">{smallDescription}</p>
+          <p className="text-gray-600 text-xs mb-2 line-clamp-4">{smallDescription}</p>
         </div>
       </div >
     </>
